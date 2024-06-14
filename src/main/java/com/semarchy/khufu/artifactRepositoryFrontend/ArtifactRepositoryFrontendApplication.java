255a32978474d765bf07e9511388c2a72af9e980
fdeze <fabien.dezempte@semarchy.com>
2024-06-14 14:46:00 +0200

    init project

---
+++ src/main/java/com/semarchy/khufu/artifactRepositoryFrontend/ArtifactRepositoryFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.semarchy.khufu.artifactRepositoryFrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifactRepositoryFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifactRepositoryFrontendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/semarchy/khufu/artifactRepositoryFrontend/connectorRepository/api/v1/service/ConnectorRepositoryContants.java
@@ -0,0 +1,9 @@
+package com.semarchy.khufu.artifactRepositoryFrontend.connectorRepository.api.v1.service;
+
+public class ConnectorRepositoryContants {
+
+	public static final String ARTIFACT_NAME = "artifactName";
+	public static final String ARTIFACT_TYPE = "artifactType";
+	public static final String ARTIFACT_VERSION = "artifactVersion";
+
+}
