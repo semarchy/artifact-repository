1269552ceeb222cdd64926612b3f209d3979e8e2
fdeze <fabien.dezempte@semarchy.com>
2024-06-27 11:00:23 +0200

    -cleanup

--- connector-repository/src/main/java/com/semarchy/khufu/artifactRepositoryFrontend/connectorRepository/api/v1/service/ConnectorRepositoryContants.java
+++
@@ -1,9 +0,0 @@
-package com.semarchy.khufu.artifactRepositoryFrontend.connectorRepository.api.v1.service;
-
-public class ConnectorRepositoryContants {
-
-	public static final String ARTIFACT_NAME = "artifactName";
-	public static final String ARTIFACT_TYPE = "artifactType";
-	public static final String ARTIFACT_VERSION = "artifactVersion";
-
-}
